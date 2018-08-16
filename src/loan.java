3c1be6f251f757eaa741415fc6aba3c03f385636
gujjurocks <41317723+gujjurocks@users.noreply.github.com>
2018-08-16 10:54:03 +1000

    Change varibale name
    ID -> id
    B to book
    M to member
    D to dueDate
    state to loanSatet
    sb to stringBuilder
    sdf simpleDateFormate

--- src/loan.java
+++ src/loan.java
@@ -7,25 +7,25 @@ public class loan implements Serializable {
 	
 	public static enum LOAN_STATE { CURRENT, OVER_DUE, DISCHARGED };
 	
-	private int loan_id; // Change variable name from ID to loan_id
-	private book B;
-	private member M;
-	private Date D;
+	private int id; // Change variable name from ID to id
+	private book book; // Change  variable name from B to book.
+	private member member;	// Change variable name from M to member.
+	private Date dueDate;	// Change variable name from D to dueDate
 	private LOAN_STATE state;
 
 	
 	public loan(int loanId, book book, member member, Date dueDate) {
-		this.ID = loanId;
-		this.B = book;
-		this.M = member;
-		this.D = dueDate;
+		this.id = loanId;
+		this.book = book;	// Change variable name from B to book.
+		this.member = member;	//Change variable name from M to member
+		this.dueDate = dueDate;	// Change variable name from D to dueDate
 		this.state = LOAN_STATE.CURRENT;
 	}
 
 	
 	public void checkOverDue() {
 		if (state == LOAN_STATE.CURRENT &&
-			Calendar.getInstance().Date().after(D)) {
+			Calendar.getInstance().Date().after(dueDate)) { // Change variable name from D to dueDate
 			this.state = LOAN_STATE.OVER_DUE;			
 		}
 	}
@@ -36,28 +36,28 @@ public class loan implements Serializable {
 	}
 
 	
-	public Integer getId() {
-		return ID;
+	public Integer getId() { 
+		return id;	// change variable name from ID to id
 	}
 
 
 	public Date getDueDate() {
-		return D;
+		return dueDate;	//// Change variable name from D to dueDate
 	}
 	
 	
 	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat simpleDateFormate = new SimpleDateFormat("dd/MM/yyyy"); // Change from sdf to simpleDateFormate
 
-		StringBuilder sb = new StringBuilder();
-		sb.append("Loan:  ").append(ID).append("\n")
-		  .append("  Borrower ").append(M.getId()).append(" : ")
+		StringBuilder stringBuilder = new StringBuilder();	// Change variable name from sb to stringBuilder
+		stringBuilder.append("Loan:  ").append(id).append("\n")// Change variable name from sb to stringBuilder and ID to id
+		  .append("  Borrower ").append(member.getId()).append(" : ") // Change variable ma,e M to member
 		  .append(M.getLastName()).append(", ").append(M.getFirstName()).append("\n")
-		  .append("  Book ").append(B.ID()).append(" : " )
+		  .append("  Book ").append(book.id()).append(" : " ) // Change varibale name from ID to id nad B to book
 		  .append(B.Title()).append("\n")
-		  .append("  DueDate: ").append(sdf.format(D)).append("\n")
+		  .append("  DueDate: ").append(simpleDateFormate.format(D)).append("\n")	// Change from sdf to simpleDateFormate
 		  .append("  State: ").append(state);		
-		return sb.toString();
+		return stringBuilder.toString();	//Change variable name from sb to stringBuilder
 	}
 
 
